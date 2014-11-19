696389b81032f6289d8c8ed49dc7c10413c32462
canes6288 <joshkushner1@gmail.com>
2014-11-19 17:28:37 -0500

    more work in addsubmission/questions

--- app/scripts/controllers/addsubmission.js
+++ app/scripts/controllers/addsubmission.js
@@ -6,4 +6,34 @@ angular.module('kaboodleApp')
 
   $scope.selectedContest = contestService.selectedContest;
 
+  $scope.numQuestions = function() {
+    return $scope.selectedContest.question_file.length;
+  };
+
+  $scope.currentQuestion = $scope.selectedContest.question_file[0];
+
+  $scope.answers = [];
+  $scope.questions = [];
+
+  //pushing answer into answers array and resetting currentQuestion = question_file[next questions index number]
+  $scope.goToNextQuestion = function (answer) {
+    if ($scope.currentQuestion.question_number < $scope.numQuestions()) {
+      $scope.answers.push(answer);
+      $scope.questions.push($scope.currentQuestion.label)
+      $scope.currentQuestion =
+      $scope.selectedContest.question_file[$scope.currentQuestion.question_number];
+      $scope.questionForm.$setPristine();
+   }
+  };
+
+  $scope.isDone = function() {
+    return $scope.currentQuestion.question_number === $scope.numQuestions();
+  };
+
+  $scope.isStepDone = function(stepNumber) {
+    return stepNumber < $scope.currentQuestion.question_number;
+  };
+
+
+
 }]);
